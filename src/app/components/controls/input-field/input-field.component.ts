3d233de25267b43518f8cf82dac2a0ce35689780
Nouran Awad <nawad@onica.com>
2022-01-01 20:39:31 +0200

    Toggle passwords visibility

--- src/app/components/controls/input-field/input-field.component.ts
+++ src/app/components/controls/input-field/input-field.component.ts
@@ -4,6 +4,7 @@ import {
   Input,
   OnInit,
   Output,
+  SimpleChanges,
   ViewEncapsulation,
 } from '@angular/core';
 
@@ -14,11 +15,12 @@ import {
   encapsulation: ViewEncapsulation.None,
 })
 export class InputFieldComponent implements OnInit {
-  @Input() myId: string;
   @Input() bindModelData: any;
   @Input() label: string;
   @Input() name: string;
   @Input() type: string = 'text';
+  icon: string;
+  fieldType: string;
 
   // note that this must be named as the input name + "Change"
   @Output() bindModelDataChange: any = new EventEmitter();
@@ -31,4 +33,22 @@ export class InputFieldComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.type) {
+      this.fieldType = changes.type.currentValue;
+
+      if (changes.type.currentValue === 'password') {
+        this.icon = 'visibility_off';
+      } else {
+        this.icon = '';
+      }
+    }
+  }
+
+  toggleVisibility() {
+    this.fieldType = this.fieldType === 'text' ? 'password' : 'text';
+    this.icon =
+      this.icon === 'visibility_off' ? 'visibility' : 'visibility_off';
+  }
 }
--- src/app/modules/material-ui/material-ui.module.ts
+++ src/app/modules/material-ui/material-ui.module.ts
@@ -4,8 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
-const modules = [MatFormFieldModule, MatInputModule, MatButtonModule];
+const modules = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatIconModule,
+];
 
 @NgModule({
   declarations: [],
