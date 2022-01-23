e271f67edba0a79f22735539b45d599914aba05c
Nouran Awad <nawad@onica.com>
2022-01-23 15:16:45 +0200

    Finish cart menu UI

--- src/app/components/MainLayout/main-layout.component.ts
+++ src/app/components/MainLayout/main-layout.component.ts
@@ -2,6 +2,9 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { CartProducts, MenuItem } from 'src/app/models/Restaurants';
+import { selectCartProducts } from 'src/app/store/selectors/cart.selector';
 
 @Component({
   selector: 'app-main-layout',
@@ -11,15 +14,39 @@ import { Router } from '@angular/router';
 })
 export class MainLayoutComponent implements OnInit {
   @Input() currentUser: firebase.default.User | null;
+  cartProducts: CartProducts;
+  totalPrice: number;
 
-  constructor(private auth: AngularFireAuth, private router: Router) {}
+  get cartItemsCount() {
+    return Object.keys(this.cartProducts).length;
+  }
 
-  ngOnInit(): void {}
+  constructor(
+    private auth: AngularFireAuth,
+    private router: Router,
+    private store: Store
+  ) {}
+
+  ngOnInit(): void {
+    this.store.select(selectCartProducts).subscribe((res) => {
+      this.cartProducts = res.products;
+      this.totalPrice = this.getTotal();
+    });
+  }
 
   logout() {
     this.auth.signOut().then(() => {
-      // this.store.dispatch(addCurrentUser({ user: null }));
       this.router.navigate(['login']);
     });
+  }
+
+  getTotal() {
+    let total: number = 0;
+
+    Object.values(this.cartProducts).map((prod) => {
+      total += prod.price * prod.quantity;
+    });
+
+    return total;
   }
 }
